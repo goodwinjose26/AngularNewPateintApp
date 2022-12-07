0eaef488fd7920f6dd88256bd04759ec13499c77
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-07 08:14:38 +0530

    add

---
+++ src/app/add/add.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css']
+})
+export class AddComponent {
+
+  name=""
+  age=""
+  mobno=""
+  address=""
+  constructor(private api:ApiService) {}
+
+  addpateint=()=>
+  {
+    let data:any={"name":this.name,"age":this.age,"mobno":this.mobno,"address":this.address}
+    console.log(data)
+    this.api.addpateint(data).subscribe(
+      (response:any)=>
+      {
+        console.log(response)
+      }
+    )
+  }
+
+}
